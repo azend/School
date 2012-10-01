5898522aafdf6761a817f2f2c0472005ee5c4d94
Verdi R-D <verdi@azend.org>
2012-10-01 09:09:59 -0400

    A newer copy of my local repo

--- PROG1345 - C Programming/Projects/cA1b/cA1b/cA1b.c
+++ PROG1345 - C Programming/Projects/cA1b/cA1b/cA1b.c
@@ -1,7 +1,153 @@
 
+/* Assignment #1B
+ * Name: Verdi R-D
+ * Date: Sept 6th, 2012
+ * Comment: I don't know C. This was just me playing around
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Enable for debug output. Lets you trace a stack trace more easily.
+#define DEBUG 0
+
+// Preferrably not handled by the precompiler but I also didn't want to hard code it.
+#define NUM_BRIEFCASES 26
+
+int numbriefcases = 26;
+int bankerRounds = 6;
+
+struct briefcase {
+	// Money inside the briefcase
+	int amount;
+
+	// C doesn't natively support booleans... Why I don't understand
+	int opened;
+};
+
+void populatebriefcases (struct briefcase briefcases [], int numbriefcases) {
+	int i;
+
+	srand(1);
+
+	if (DEBUG) printf("briefcases:\r\n");
+	for (i = 0; i < numbriefcases; i++ ) {
+		briefcases[i].amount = rand();
+		briefcases[i].opened = 0;
+		if (DEBUG) printf("Suitecase %d: %d\r\n", i + 1, briefcases[i].amount);
+	}
+
+	return;
+}
+
+void printClosedBriefcases (struct briefcase briefcases [], int numbriefcases) {
+	int x;
+
+	for (x = 0; x < numbriefcases; x++) {
+		// If the briefcase is closed, print it's number
+		if (briefcases[x].opened == 0) printf("%d ", x + 1);
+	}
+}
+
+int checkBriefcase (struct briefcase briefcases[], int briefcase) {
+	if (!briefcases[briefcase].opened) return 1;
+	else return 0;
+}
+
+void openBriefcase (struct briefcase briefcases[], int briefcase) {
+	briefcases[briefcase - 1].opened = 1;
+	printf("There is %d in briefcase %d", briefcases[briefcase -1].amount, briefcase);
+	return;
+}
+
+int getInt (char * question) {
+	int answer = 0;
+	do {
+		printf("%s: ", question);
+		answer = getNum();
+		printf("\r\n");
+	} while (!answer);
+
+	return 0;
+}
+
+int getYesNo (char * question) {
+	char* answer;
+	do {
+		printf("%s [y/n]: ", question);
+		sscanf(answer, "%c");
+		printf("\r\n");
+	} while (0);
+
+	return 0;
+}
+
+int gameLoop (struct briefcase briefcases [], int numbriefcases) {
+	int sum = 0;
+	int roundSum = 0;
+	int selectedBriefcase = 0;
+
+	int x;
+	int y;
+
+	for (x = bankerRounds; x > 0; x--) {
+		for (y = 0; y < x; y++) {
+			// Open some briefcases
+			printf("Briefcases left: ");
+			printClosedBriefcases(briefcases, NUM_BRIEFCASES);
+			printf("\r\n");
+
+			selectedBriefcase = getInt("Which briefcase would you like to open?");
+			printf("%d\r\n", selectedBriefcase);
+			if (checkBriefcase(briefcases, selectedBriefcase)) openBriefcase(briefcases, selectedBriefcase);
+
+
+
+
+		}
+		// Banker makes an offer
+
+	}
+
+	return 0;
+}
+
+void startGame (void) {
+	struct briefcase briefcases [NUM_BRIEFCASES];
+	populatebriefcases(briefcases, NUM_BRIEFCASES);
+
+	gameLoop(briefcases, NUM_BRIEFCASES);
+
+}
+
+
+// Straight from the page
+int getNum(void)
+{
+
+	/* the array is 121 bytes in size; we'll see in a later lecture how we can improve this code */
+	char record[121] = {0};                      /*  record stores the string */
+	int number = 0;
+
+	/* NOTE to  student: indent and brace this function consistent with  your others */
+	/* use  fgets() to get a string from the keyboard */
+	fgets(record,  121, stdin);
+
+	/* extract  the number from the string; sscanf() returns a number 
+	* corresponding with the number of items it found in the string */
+	if(  sscanf(record, "%d", &number) != 1 )
+	{
+		/*  if the user did not enter a number recognizable by 
+			* the system, set number to -1 */
+		number  = -1;
+	}
+
+	return  number;
+}
+
 
 int main (void) {
-
+	startGame();
 
 	return 0;
 }
---
+++ PROG1345 - C Programming/Projects/cA2/cA2/cA2/cA2.c
@@ -0,0 +1,156 @@
+
+/* 
+ *  FILE          : cA2.c
+ *  PROJECT       : PROG1345 - Assignment #2
+ *  PROGRAMMER    : Verdi R-D
+ *  FIRST VERSION : 2012-09-25
+ *  DESCRIPTION   :
+ *    The functions in this file are used to calculate the
+ *    power of a given number by it's exponent and to check
+ *    whether a number is within a given range.
+ */
+
+#include <stdio.h>
+
+// Pre-compiler defined debug condition. Only really useful for me (author).
+#define DEBUG 0
+
+//
+// FUNCTION PROTOTYPES
+//
+int checkRange (int input, int rangeMin, int rangeMax);
+int feelThePower(int base, int exponent);
+int getNum(void);
+int main (void);
+
+/*
+ * FUNCTION      : checkRange
+ * DESCRIPTION   :
+ *   This function decides whether a given
+ *   input is within a given maximum and 
+ *   minimum range.
+ * PARAMETERS    :
+ *   int input   : The value to check
+ *   int rangeMin: Range bottom end or minimum value
+ *   int rangeMax: Range top end or maximum value
+ * RETURNS       :
+ *   int : returns the result of the check. 1 if
+ *            the input is inside the range and 0
+ *            if it is not.
+ */
+int checkRange (int input, int rangeMin, int rangeMax) {
+	// Define and assign the return value to it's false state
+	int result = 0;
+
+	// Check whether the input is within the predefined range
+	if ( input >= rangeMin && input <= rangeMax ) {
+		if (DEBUG) printf("%d is inside the range of %d to %d\r\n", input, rangeMin, rangeMax);
+
+		// Return result as 1 since it is within its range
+		result = 1;
+	}
+	else {
+		// Return result as 0 since it is not within its range
+		if (DEBUG) printf("%d is not inside the range of %d to %d\r\n", input, rangeMin, rangeMax);
+		// Since result has been already assigned 0, there is nothing to do here
+	}
+
+	return result;
+}
+
+/*
+ * FUNCTION      : feelThePower
+ * DESCRIPTION   :
+ *   This function calculates an algebraic
+ *   power using a passed base and exponent.
+ * PARAMETERS    :
+ *   int base    : The power's base
+ *   int exponent: Power's exponent
+ * RETURNS       :
+ *   int : total after performing an algebraic
+ *            power function using the passed
+ *            base and exponent.
+ */
+int feelThePower(int base, int exponent) {
+	int result = 1;
+	int i = 0;
+
+	result = base;
+	
+	for (i = 0; i < exponent - 1; i++) {
+		result *= base;
+	}
+
+	return result;
+}
+
+
+/*
+ * FUNCTION      : getNum
+ * DESCRIPTION   :
+ *   This function gets a number from the user
+ *   using stdin
+ * PARAMETERS    :
+ *   void        : 
+ * RETURNS       :
+ *   int : The number the user entered formatted
+ *            as an integer.
+ */
+int getNum(void) {
+	/* the array is 121 bytes in size; we'll see in a later lecture how we can improve this code */
+	char record[121] = {0};                      /*  record stores the string */
+	int number = 0;
+	/* NOTE to  student: indent and brace this function consistent with  your others */
+	/* use  fgets() to get a string from the keyboard */
+	fgets(record,  121, stdin);
+	/* extract  the number from the string; sscanf() returns a number 
+	* corresponding with the number of items it found in the string */
+	if(  sscanf(record, "%d", &number) != 1 ) {
+		/*  if the user did not enter a number recognizable by 
+		* the system, set number to -1 */
+		number  = -1;
+	}
+	return  number;
+}
+
+int main (void) {
+	// Assign default values to variables base and exponent
+	int base = 5, exponent = 2;
+
+	// Create an input buffer for deciding whether or not to 
+	// use inputted values
+	int input = 0;
+
+	while(1) {
+		// Get the base from the user
+		printf("Please enter a base: ");
+		input = getNum();
+
+		// Check if it is between our preset range
+		if (checkRange(input, 1, 25)) {
+			// If it is, make it our base
+			base = input;
+		}
+		else {
+			// If it isn't, ignore it and use default values
+			printf("The value %d is out of range. Defaulting to %d\r\n", input, base);
+		}
+
+		// Get the base from the user
+		printf("Please enter an exponent: ");
+		input = getNum();
+
+		// Check if it is between our preset range
+		if (checkRange(input, 1, 5)) {
+			// If it is, make it our base
+			exponent = input;
+		}
+		else {
+			// If it isn't, ignore it and use default values
+			printf("The value %d is out of range. Defaulting to %d\r\n", input, exponent);
+		}
+
+		// Print the calculated power
+		printf("%d to the power of %d is %d\r\n\r\n", base, exponent, feelThePower(base, exponent));
+	}
+}
