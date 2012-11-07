f1a1cbc7d706cbf62e6861e98aa1dc515b9cddfa
Verdi R-D <verdi@azend.org>
2012-11-07 09:40:01 -0500

    Updated c4c

--- PROG1345 - C Programming/Projects/c4c/c4c/c4c.c
+++ PROG1345 - C Programming/Projects/c4c/c4c/c4c.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_AIRPORTS 128
+#define MAX_FLIGHTS 128
 
 typedef struct {
-	int id;
+	int destinationAirportId;
 	int departureTime; // In minutes since 00:00
 	int flightLength; // In minutes since 00:00
-} flight;
+} Flight;
 
 typedef struct {
-	int id;
 	char * name;
-	flight * departures;
+	Flight departures [MAX_FLIGHTS];
+	int timezone;
 	int visited; // 0 for no 1 for yes
-} city;
+} Airport;
+
+
+void populateAirports (Airport airports[]) {
+
+	char* csv = "This, is, an, example, csv";
+
+	char* ptr = strtok(csv, ",");
+
+	char* destinationName = (char*) *ptr;
+
+	while (ptr != NULL) {
+		
+		ptr = strtok(NULL, ",");
+	}
+
+}
 
 int main () {
-	
+	Airport airports[MAX_AIRPORTS];
+
+	populateAirports(airports);
+
 	return 0;
 }
