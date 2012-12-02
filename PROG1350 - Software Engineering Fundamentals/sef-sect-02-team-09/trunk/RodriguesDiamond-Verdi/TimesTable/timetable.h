44e82e34ab91b9d9ecfe79548cd22c9fdca6fa4d
Verdi R-D <verdi@azend.org>
2012-12-02 14:40:26 -0500

    Added Sean's timestable assignment

--- PROG1350 - Software Engineering Fundamentals/sef-sect-02-team-09/trunk/RodriguesDiamond-Verdi/TimesTable/timetable.h
+++
@@ -1,7 +0,0 @@
-/* ======================================================= */
-/* File    : timetable.h                                   */
-/* Purpose : This file contains the prototype for the      */
-/*           actual calculateTimesTable() function         */
-/* ======================================================= */
-
-void calculateTimesTable(int answers[], int whichTimeTable);
